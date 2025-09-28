b6da337aa94f050e3c7c05d831cddedf8af25bca
solomon-webb <towolawisolomon09@gmail.com>
2025-09-28 23:09:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'calculator-cache-v1';
+const urlsToCache = [
+  '/calculator/',
+  '/calculator/index.html',
+  '/calculator/style.css',
+  '/calculator/script.js',
+  '/calculator/images/screenshot-narrow.png',
+  '/calculator/images/screenshot-wide.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Optional: handle caching here
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
