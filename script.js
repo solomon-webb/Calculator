9cadc5ffa0d33c2441ee68416f3008eff225482e
solomon-webb <towolawisolomon09@gmail.com>
2025-09-28 22:57:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+  let display = document.getElementById('disp');
+    let justCalculated = false;
+
+    function press(value) {
+      if (justCalculated) {
+        display.value = '';
+        justCalculated = false;
+      }
+      display.value += value;
+    }
+
+    function calculate() {
+      try {
+        display.value = eval(display.value);
+        justCalculated = true;
+      } catch {
+        display.value = "Syntax Error";
+        justCalculated = true;
+      }
+    }
+
+   
+    function clearDisplay() {
+      display.value = "";
+      justCalculated = false;
+    }
+  function deletee() {
+     if (!justCalculated) {
+        display.value = display.value.slice(0, -1);
+      }
+  }
+
+
+  if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js").then(() => {
+    console.log("Service Worker Registered");
+  });
+}
+
+window.addEventListener('beforeinstallprompt', (e) => {
+     e.prompt(); // Triggers the prompt
+   });
+
+   if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('Service Worker Registered'));
+  });
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+   
